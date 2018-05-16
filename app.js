bcad88881b0ac3404a97692a3f1823ea3ce42e78
Vinayak <vinayakkaushikk@gmail.com>
2018-05-16 11:31:14 +0530

    created a watchFace function

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-var long = 25;
-var short = 5;
-var count = 4;
+let long = 2000;
+let short = 1;
+let count = 4;
 
-var display = document.getElementById("display");
-var button = document.getElementById("button");
+let display = document.getElementById("display");
 let span = document.getElementById("eventHolder");
+let i = document.querySelector("i");
 
-var event = new Event('timeOut');
+let event = new Event('timeOut');
 
 let timer = {
 	state: "init",
 	time: 10
 }
 
-timer.counter = function (time) {
-	console.log('the time is: ' + time.toString());
-	timer.time = time;
-	setTimeout(function() {
-		count.innerHTML = timer.time.toString();
-		console.log(timer.time);
-		timer.time-=1
+timer.runCounter = function (time) {
+	timer.time = time
+	setTimeout( function() {
+		var face = watchFace(time)
+		display.innerHTML = pad(face.a,2).toString() + ":" + pad(face.b,2).toString()
+		console.log(time);
+		timer.time = time - 1
 		if ( timer.time > -1 ) {
-			timer.counter();
+			timer.runCounter(timer.time);
 		}
 		else {
 			span.dispatchEvent(event);
@@ -34,11 +34,13 @@ span.addEventListener("timeOut", function() {
 		// State change to longDone
 		timer.state = "longDone";
 		console.log(timer.state)
+		toggleButtonClass();
 	}
 	if (timer.state == "short") {
 		// State change to shortDone
 		timer.state = "shortDone";
 		console.log(timer.state)
+		toggleButtonClass();
 	}
 });
 
@@ -50,18 +52,50 @@ function buttonPress() {
 	// short => The state when short is going on
 	// shortDone => The state when the short timer is done
 	if (timer.state == "init") {
-		timer.counter(long);
+		timer.runCounter(long);
 		timer.state = "long";
+		toggleButtonClass();
 	}
 	if (timer.state == "longDone") {
-		timer.counter(short);
+		timer.runCounter(short);
 		timer.state = "short";
+		toggleButtonClass();
 	}
 	if (timer.state == "shortDone") {
-		timer.counter(long);
+		timer.runCounter(long);
 		timer.state = "long";
+		toggleButtonClass();
 	}
 }
 
-button.addEventListener("click", buttonPress);
+function toggleButtonClass() {
+	if (i.classList.contains("fa-play-circle")) {
+		i.classList.remove("fa-play-circle");
+		i.classList.add("fa-stop-circle");
+	}
+	else {
+		i.classList.add("fa-play-circle");
+		i.classList.remove("fa-stop-circle");
+	}
+}
 
+i.addEventListener("click", buttonPress);
+
+function watchFace(sec) {
+	if (sec > 3599) {
+		var a = parseInt(sec / 3600); // hours
+		var b = parseInt((sec % 3600) / 60); // mins
+		return {a,b}
+	}
+	else {
+		var a = parseInt(sec / 60) // mins
+		var b = sec % 60; // secs
+		return {a,b}
+	}
+}
+
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
